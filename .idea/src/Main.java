c8551549d7c71c0e4223a585e25c01e67232839b
huseynasgarov85 <huseynnaskerovv@gmail.com>
2024-05-17 01:09:26 +0400

    reload file

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a=5;
+        int b=6;
+        int c=a+b;
+        System.out.println(c);
     }
 }
