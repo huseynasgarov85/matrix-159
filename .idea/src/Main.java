d929bf66a7ef6edc299800a5ea42afcc2d5f7184
huseynasgarov85 <huseynnaskerovv@gmail.com>
2024-05-17 23:10:52 +0400

    consider huseyn branch

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int a=5;
-        int b=6;
-        int c=a+b;
-        System.out.println(c);
-        int[] array =new int[2];
+        String name = "Huseyn";
+        String surname = "Asgarov";
+        System.out.println(name + " " + surname);
     }
 }
