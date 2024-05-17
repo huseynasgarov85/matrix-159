7584c86a6c754fef9b542d4de7f95c84691003ac
huseynasgarov85 <huseynnaskerovv@gmail.com>
2024-05-17 15:07:55 +0400

    writin in Integer code

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String name="huseynn";
-        System.out.println(name);
+        Integer i = 5;
+        System.out.println(i);
     }
 }
